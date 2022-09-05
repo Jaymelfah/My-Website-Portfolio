f0cec7fa82bc03b876488219d1d81b0971afaec1
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-09-05 10:41:35 +0000

    Style hamburger menu

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const hamburgerPopup = document.querySelector('.nav-toggle');
+const menuIcons = document.querySelectorAll('.menu_icons > a');
+
 hamburgerPopup.addEventListener('click', ()=>{
     document.body.classList.toggle('hamburger-open')
+})
+
+menuIcons.forEach(link => {
+    link.addEventListener('click', ()=> {
+        document.body.classList.remove('hamburger-open')
+    })
 })
